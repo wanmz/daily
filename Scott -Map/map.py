8c06a21f755910c0f5fa3d4da6f32a81c1070320
wanmz <289241101@qq.com>
2021-03-06 21:15:59 +0800

    高德地图API

--- Scott -Map/map.py
+++ Scott -Map/map.py
@@ -15,7 +15,35 @@ import requests
 from urllib import parse
 
 
-class Map(object):
+class District(object):
+    def __init__(self):
+        self.url = "https://restapi.amap.com/v3/config/district"
+        self.key = "b4dc36fadc284db9b877006f7f3d9f60"
+        self.keywords = ""
+
+    def get_data(self):
+        # subdistrict 0：不返回下级行政区；1：返回下一级行政区；2：返回下两级行政区；3：返回下三级行政区；
+        data = {"keywords": self.keywords, "key": self.key, "extensions": "base", "output": "json", "subdistrict": 0}
+        res = requests.get(self.url, data).json()
+        return res
+
+    # https://developer.amap.com/api/webservice/guide/api/district
+    def del_district(self, res):
+        if res["status"] != '1':
+            print("Sorry {0} 行政区获取ERROR!!!".format(self.keywords))
+            return
+        else:
+            for _res in res["districts"]:
+                return {"citycode": _res["citycode"], "adcode": _res["adcode"], "center": _res["center"]}
+
+    def run(self, *args):
+        self.keywords = args[0]
+        res = self.get_data()
+        msg = self.del_district(res)
+        return msg
+
+
+class MapWeather(object):
     def __init__(self):
         self.url = "https://restapi.amap.com/v3/weather/weatherInfo"
         self.key = "b4dc36fadc284db9b877006f7f3d9f60"
@@ -45,6 +73,14 @@ class Map(object):
 
 
 if __name__ == '__main__':
-    code = input("请输入地区编码: ")
-    map = Map()
-    map.run(code)
+    # weather = MapWeather()
+    # code = input("请输入地区编码: ")
+    # weather.run(code)
+
+    district = District()
+    keywords = input("请输入城市: ")
+    msg = district.run(keywords)
+    # print(msg)
+
+    weather = MapWeather()
+    weather.run(msg["adcode"])
