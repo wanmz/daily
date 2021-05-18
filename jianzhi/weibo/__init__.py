27373bd31fc0b0f850b92022a906098e87801daf
wanmz <289241101@qq.com>
2021-05-18 23:25:58 +0800

    微博情感分析fix

--- jianzhi/weibo/__init__.py
+++ jianzhi/weibo/__init__.py
@@ -17,4 +17,6 @@ https://blog.csdn.net/hzp666/article/details/78969150
 https://blog.csdn.net/weixin_43717839/article/details/98073726
 https://www.bilibili.com/video/av412884801/
 https://github.com/ReainL/sina_comment
+https://m.weibo.cn/single/rcList?format=cards&id=4627963633205346&type=comment&hot=0&page=2
+https://m.weibo.cn/comments/hotflow?id=4627963633205346&mid=4627963633205346&max_id_type=0
 """
